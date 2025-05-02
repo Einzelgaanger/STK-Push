4a0bd4308bd509039377d582b42b2424d1ced7be
Einzelgaanger <al.mulinge.ke@gmail.com>
2025-05-02 07:49:43 +0300

    01

--- stk_push.php
+++ stk_push.php
@@ -3,18 +3,16 @@ if(isset($_POST['submit'])){
     date_default_timezone_set('Africa/Nairobi');
 
     # access token
-    $consumerKey = 'C6W5OcNHxunnIgtGEAWUA3o9TlIfkLAb';
-    $consumerSecret = '3coRAVLjOwjG0Hts';
+    $consumerKey = getenv('CONSUMER_KEY');
+    $consumerSecret = getenv('CONSUMER_SECRET');
     
-
-    $BusinessShortCode = '174379';
-    $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';  
-    
+    $BusinessShortCode = getenv('BUSINESS_SHORTCODE');
+    $Passkey = getenv('PASSKEY');  
     
     $PartyA = $_POST['phonenumber']; //Phone number, 
     $AccountReference = 'Pio Spices East Africa';
     $TransactionDesc = 'Test lipa na mpesa stk push initiation';
-    $Amount = $_POST['amount'];;
+    $Amount = $_POST['amount'];
     
     $Timestamp = date('YmdHis');    
     
@@ -26,20 +24,20 @@ if(isset($_POST['submit'])){
     $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
     # callback url
-    $CallBackURL = 'https://stk-push-php.herokuapp.com/callback.php';  
+    $CallBackURL = 'https://' . $_SERVER['HTTP_HOST'] . '/callback.php';  
 
     $curl = curl_init($access_token_url);
-	//encode to base64
-	$credentials = base64_encode($consumerKey.":".$consumerSecret);
-	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$credentials));
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-	curl_setopt($curl, CURLOPT_HEADER, FALSE);
-	curl_setopt($curl, CURLOPT_USERPWD,$consumerKey.":".$consumerSecret); 
-	//excecute curl request
-	$result = curl_exec($curl);
-	$status = curl_getinfo($curl,CURLINFO_HTTP_CODE);
-	$result = json_decode($result);
-	$accessToken = $result->access_token; 
+    //encode to base64
+    $credentials = base64_encode($consumerKey.":".$consumerSecret);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$credentials));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($curl, CURLOPT_HEADER, FALSE);
+    curl_setopt($curl, CURLOPT_USERPWD,$consumerKey.":".$consumerSecret); 
+    //excecute curl request
+    $result = curl_exec($curl);
+    $status = curl_getinfo($curl,CURLINFO_HTTP_CODE);
+    $result = json_decode($result);
+    $accessToken = $result->access_token; 
    
     curl_close($curl);
     # header for stk push
@@ -72,7 +70,7 @@ if(isset($_POST['submit'])){
     print_r($curl_response);
 
     echo $curl_response;
-}else {
+} else {
     echo 'error';
 }
 ?>
